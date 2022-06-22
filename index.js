b5e2aa05befcfef30fe93e355b33a5c0e801a79c
Will Gundy <willgunderson503@gmail.com>
2022-06-22 10:09:56 -0700

    finished some dsna function

--- index.js
+++ index.js
@@ -42,3 +42,20 @@ console.log(every([2, 4, 6], n => n % 2 === 0));
 //true
 console.log(every([1, 2, 3], n => n % 2 === 0));
 //false
+
+//exercise 4 some
+function some(arr, predicate) {
+  for(let i = 0; i < arr.length; i++) {
+    const item = arr[i];
+    if (predicate(item)) {
+      return true;
+      break;
+    }
+  }
+  return false;
+}
+
+console.log(some([1, 6, 5], n => n % 2 === 0));
+//true
+console.log(some([1, 7, 3], n => n % 2 === 0));
+//false
